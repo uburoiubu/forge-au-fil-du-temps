3ebd6572bfb41105a4664c382b483892e277ceaf
Evgenii Kozlov <enklave.mare.balticum@protonmail.com>
2025-08-20 22:02:47 +0200

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-              <p>
-                <b>La Forge au fil du temps</b> est une association dont le but est de valoriser et d'animer l’ancienne forge de Bourzolles à Souillac.
-              </p>
+        <Image
+          src="/images/forge-vero-van-be.jpg" // path relative to /public
+          alt="Ancienne forge à Souillac"
+          width={800}
+          height={500}
+          className="rounded-lg"
+        />
+        <p>
+          <b>La Forge au fil du temps</b> est une association dont le but est de
+          valoriser et d'animer l’ancienne forge de Bourzolles à Souillac.
+        </p>
       </main>
     </div>
   );
