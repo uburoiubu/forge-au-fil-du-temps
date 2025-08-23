d4580108381a3f7209377898d06af689b7c1b1cc
Evgenii Kozlov <enklave.mare.balticum@protonmail.com>
2025-08-23 14:10:49 +0200

    update images

--- app/visiter/page.tsx
+++ app/visiter/page.tsx
@@ -1,18 +1,22 @@
 import Image from "next/image";
 
-export default function HistoryPage() {
+export default function VisiterPage() {
   return (
     <main className="min-h-screen text-justify">
-      <div className="w-full h-[400px] relative">
+      {/* Responsive banner */}
+      <div className="w-full relative h-[25vh] sm:h-[300px] md:h-[400px]">
         <Image
-          src="/images/plafond.jpg"
+          src="/images/visiter.jpg"
           alt="Plafond de la maison du maître"
           fill
           className="object-cover"
+          priority
         />
       </div>
-       <h1 className="text-5xl text-center p-6">Visiter</h1>
-      <div className="text-xl leading-relaxed pr-10 pl-10">
+
+      <h1 className="text-5xl text-center p-6">Visiter</h1>
+
+      <div className="text-xl leading-relaxed px-10">
         <p>
           <b>Bienvenue à la Forge</b>
         </p>
@@ -38,7 +42,7 @@ export default function HistoryPage() {
 
         <p>
           En vous remerciant, je vous souhaite une belle visite un peu «
-          labyrinthique » mais qui, je l'espère, vous fera rêver !
+          labyrinthique » mais qui, je l&apos;espère, vous fera rêver !
         </p>
 
         <p className="text-right">
