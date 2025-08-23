8cf0cc8a7c52cd917088a00dae4e96c7eb64b8dc
Evgenii Kozlov <enklave.mare.balticum@protonmail.com>
2025-08-23 16:00:10 +0200

    added texts

--- app/histoire/forges/page.tsx
+++ app/histoire/forges/page.tsx
@@ -2,17 +2,107 @@ import Image from "next/image";
 
 export const metadata = {
   title: "Les forges - Histoire & patrimoine - La Forge au fil du temps",
-  description: "Découvrez l'histoire des forges de Bourzolles à Souillac, leur fonctionnement, leur évolution et leur impact sur le développement industriel de la région."
+  description:
+    "Découvrez l'histoire des forges de Bourzolles à Souillac, leur fonctionnement, leur évolution et leur impact sur le développement industriel de la région.",
 };
 
 export default function TourPage() {
   return (
     <main className="min-h-screen text-justify">
       <h1 className="text-5xl text-center pt-6 pr-6 pl-6">
-        Les forges
+        Les Forges de Bourzolles
       </h1>
       <div className="mt-6 space-y-2 text-xl leading-relaxed pr-10 pl-10">
+        <p>
+          Encouragée par la politique économique et les privilèges accordés par
+          les rois successifs, l'activité de forge se développe en France à
+          partir du 15<sup>ème</sup>
+          siècle, dans toutes les régions riches en cours d'eau, en bois et en
+          gisements ferrugineux.
+        </p>
 
+        <p>
+          Les seigneurs <strong>DE COURTIN</strong>, châtelains de Bourzolles,
+          établirent sur la Borrèze, leurs forges, au début du 16<sup>ème</sup>{" "}
+          siècle.
+        </p>
+
+        <p>
+          Elles étaient constituées, au départ, d'un haut fourneau et d'un
+          atelier de moulage pour la production et l'exploitation de la fonte.
+        </p>
+
+        <p>La production était utilisée&nbsp;:</p>
+        <ul className="list-disc pl-6">
+          <li>
+            au service du roi (pièces d'artillerie et boulets de canon en temps
+            de guerre)
+          </li>
+          <li>
+            pour la compagnie des Indes et les colonies (des chaudières qui
+            servaient à l'exploitation de la canne à sucre)
+          </li>
+          <li>
+            pour l'agriculture et le commerce (marmites, plaques de cheminées,
+            fers à gaufres, landiers, heurtoirs, pièces de charrettes…)
+          </li>
+        </ul>
+
+        <p>
+          Avant chaque <q>campagne de fondage</q> et avant la mise à feu, le
+          curé de la paroisse venait bénir le fourneau.
+        </p>
+
+        <p>
+          Chaque fondage durait environ 6 mois, nuit et jour. 70 à 80 ouvriers y
+          travaillaient, payés <q>à la tâche</q> :
+        </p>
+
+        <p>
+          Charbonniers, tireurs de mine, charretiers, laveurs de mine,
+          forgerons, chargeurs, mouleurs…
+        </p>
+
+        <p>
+          La production était transportée ensuite par la batellerie sur la
+          Dordogne vers Libourne et Bordeaux.
+        </p>
+
+        <p>
+          Après des périodes diverses de prospérité et de crise, elles
+          atteignirent leur apogée de 1830 à 1845, grâce à la création d'une
+          affinerie pour la production de fer, autorisée par ordonnance royale
+          le 13 mars 1828.
+        </p>
+
+        <p>
+          La politique économique du libre-échange et la création de grandes
+          usines plus perfectionnées signèrent la mort des Forges de Bourzolles.
+        </p>
+
+        <p>Elles furent fermées définitivement en 1869.</p>
+
+        <p>
+          <b>Propriétaires successifs des Forges de Bourzolles :</b>
+        </p>
+
+        <ul className="list-none pl-0">
+          <li>
+            <strong>Création :</strong> Seigneurs de Coustin jusqu'en 1725
+          </li>
+          <li>
+            <strong>Vente :</strong> Sclafer de Clary jusqu'en 1775
+          </li>
+          <li>
+            <strong>Légataire :</strong> Joseph de Coustin jusqu'en 1792
+          </li>
+          <li>
+            <strong>Vente :</strong> Denis Deltheil
+          </li>
+          <li>
+            <strong>Légataire :</strong> Jean-Baptiste Deltheil jusqu'en 1869
+          </li>
+        </ul>
       </div>
     </main>
   );
--- app/histoire/maison/page.tsx
+++ app/histoire/maison/page.tsx
@@ -2,16 +2,113 @@ import Image from "next/image";
 
 export const metadata = {
   title: "Maison du maître - Histoire & patrimoine - La Forge au fil du temps",
-  description: "Explorez l'histoire de la maison du maître de la Forge de Bourzolles à Souillac, son architecture et son évolution au fil des siècles."
+  description:
+    "Explorez l'histoire de la maison du maître de la Forge de Bourzolles à Souillac, son architecture et son évolution au fil des siècles.",
 };
 
 export default function TourPage() {
   return (
     <main className="min-h-screen text-justify">
-      <h1 className="text-5xl text-center pt-6 pr-6 pl-6">
-        Maison du maître
-      </h1>
+      <h1 className="text-5xl text-center pt-6 pr-6 pl-6">Maison du maître</h1>
       <div className="mt-6 space-y-2 text-xl leading-relaxed pr-10 pl-10">
+        <p>
+          C'est <strong>Jean-Baptiste DELTHEIL</strong>, dernier maître des
+          forges de Bourzolles, qui fait bâtir la demeure en 1825.
+        </p>
+
+        <p>
+          Il entre dans une période de grande prospérité grâce à la création de
+          l'affinerie.
+        </p>
+
+        <p>
+          Le fer, d'un usage plus courant et d'une commercialisation plus facile
+          permet de nouveaux débouchés.
+        </p>
+
+        <p>
+          Il se retrouve, d'achats en héritages, en possession d'un bloc foncier
+          de 650 ha dont deux forges, celle de Bourzolles et celle d'Ans.
+        </p>
+
+        <p>
+          À cette époque, la bâtisse est adossée au rocher, et la route passe
+          entre la maison et le canal.
+        </p>
+
+        <p>
+          Il y vit avec son épouse Anaïs (petite-fille d'Alexandre de
+          Bauharnais) et tous deux jouent un rôle social important sur la
+          commune de Souillac.
+        </p>
+
+        <p>
+          Jean-Baptiste fût pendant 20 ans président ou vice-président du
+          conseil général du Lot.
+        </p>
+
+        <p>
+          Après sa mort, sa veuve fonde à Bourzolles une école gratuite dont
+          elle payait les maîtresses et une maison de sœurs garde-malade.
+        </p>
+
+        <p>
+          C'est elle aussi, qui a souhaité l'aménagement d'une des grottes de la
+          propriété en chapelle dédiée à Notre-Dame de Sainte Espérance.
+        </p>
+
+        <p>
+          En 1887, <strong>Julien VALAT</strong>, fils d'Edouard VALAT, ami et
+          homme de confiance du couple DELTHEIL rachète la propriété.
+        </p>
+
+        <p>
+          Ingénieur en électricité, il installe une petite centrale hydraulique
+          sur l'ancien site des forges pour éclairer la demeure et son
+          magnifique parc.
+        </p>
+
+        <p>Il travaille à agrandir et à embellir la propriété.</p>
+
+        <p>Il fait ériger une tour centrale dotée de balcons à balustres.</p>
+
+        <p>
+          Il accole un jardin d'hiver sur le côté droit de la bâtisse et sur le
+          côté gauche une grande salle prolongée d'une chapelle.
+        </p>
+
+        <p>
+          On y trouve une cheminée monumentale surmontée de la devise de la
+          famille :<em>Labora, profice, ora et spera</em>.
+        </p>
+
+        <p>
+          Cette salle accueillera les nombreux ouvrages littéraires collectés
+          par les familles DELTHEIL et VALAT.
+        </p>
+
+        <p>
+          <strong>Marie-Anne LACOSTE</strong>, petite-fille de Julien VALAT,
+          crée en 1983 l'association <q>La Forge Patrimoine</q> et met à la
+          disposition du public cette magnifique bibliothèque !
+        </p>
+
+        <p>
+          Ce fonds, qui comprend aussi des cartes postales et des plaques de
+          photos, actuellement en dépôt, est consultable à la bibliothèque de
+          Souillac.
+        </p>
+
+        <p>
+          En décembre 2016, <strong>Virginie DESTREL</strong> rachète la
+          demeure, inhabitée depuis dix ans, et entreprend sa rénovation.
+        </p>
+
+        <p>
+          Elle crée l'association <q>La Forge au fil du temps</q> qui a pour but
+          de valoriser le patrimoine historique et immobilier du site et de
+          l'animer artistiquement et culturellement.
+        </p>
       </div>
     </main>
   );
