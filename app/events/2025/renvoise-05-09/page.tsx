150bdb937eca646b42a648c593f5f3973e6aa23d
Evgenii Kozlov <enklave.mare.balticum@protonmail.com>
2025-08-23 14:03:08 +0200

    fix

--- app/events/2025/renvoise-05-09/page.tsx
+++ app/events/2025/renvoise-05-09/page.tsx
@@ -2,20 +2,26 @@ import Image from "next/image";
 
 export default function HistoryPage() {
   return (
-    <main className="min-h-screen">
-      <h1 className="text-5xl text-center pt-6 pl-6 pr-6">Clémentine Renvoisé</h1>
+    <main className="h-auto">
+      <h1 className="text-5xl text-center pt-6 pl-6 pr-6">
+        Clémentine Renvoisé
+      </h1>
       <h1 className="text-2xl pr-6 pl-6 pt-1 text-center">Exposition/Vente</h1>
-       <h1 className="text-1xl text-center pt-1 pr-6 pl-6 pb-2">Entrée libre. Vernissage le 05/09 à 17h30</h1>
-      <div className="mt-2 space-y-2">
+      <h1 className="text-1xl text-center pt-1 pr-6 pl-6 pb-2">
+        Entrée libre. Vernissage le 05/09 à 17h30
+      </h1>
+      <div className="mt-2 px-12">
         <Image
           src="/images/events/050925-070925.jpeg"
           alt="La Forge"
-          width={800}
-          height={500}
-          className="mx-auto rounded-lg pr-12 pl-12"
+          fill={false}
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="mx-auto h-auto w-auto max-w-full rounded-xl shadow-md"
+          unoptimized
         />
       </div>
-
     </main>
   );
 }
